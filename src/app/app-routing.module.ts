84f4e5ee1c3412766175900e3144cfb74856538d
Nitrix - Valentin ROGER <valent.23@hotmail.fr>
2019-05-11 18:48:27 +0200

    push angular06 Quest

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { FilmListComponent } from './film-list/film-list.component';
 
 
 const ROUTES: Routes = [
   { path: 'user', component: UserProfileComponent },
   { path: 'sign-up', component: SignUpComponent },
+  { path: 'film-list', component: FilmListComponent },
   { path: '',
   redirectTo: '/user',
   pathMatch: 'full'
--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import {NgForm} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.sass']
 })
+
 export class SignUpComponent implements OnInit {
 
-  email = 'Un petit mail svp :';
+  model: Order = new Order();
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    // form submitted
+    console.log('form submitted');
+    console.log(this.model);
+  }
+}
+
+export class Order {
+  user: string;
+  contact: string;
 }
